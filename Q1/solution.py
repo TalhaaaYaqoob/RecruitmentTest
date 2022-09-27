710005dda19ea3a91986f3a2ae2b5014c757a6ae
Talha Yaqoob <talhaaayaqoob@gmail.com>
2022-09-27 23:35:43 +0500

    Update solution.py

--- Q1/solution.py
+++ Q1/solution.py
@@ -1,1 +1,12 @@
 ## Add code below with answer clearly stated
+n= 100
+fact = 1
+num = []
+sums = 0
+for i in range(1,n+1):
+    fact*=i
+num=map(int, str(fact))
+for i in num:
+    sums+=i
+    
+print(sums)
